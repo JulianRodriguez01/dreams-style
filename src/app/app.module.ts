992cbeffcaa5f88ec08abed29f2106de32725bef
julianrodriguez01 <julian.rodriguez21@uptc.edu.co>
2023-08-22 10:51:48 -0500

    implementation-index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './components/header-component/header-component.component';
+import { FooterComponentComponent } from './components/footer-component/footer-component.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { InitialComponent } from './pages/initial/initial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    FooterComponentComponent,
+    NavbarComponent,
+    InitialComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/header-component/header-component.component.ts
@@ -0,0 +1,31 @@
+import { Component, Renderer2, ElementRef  } from '@angular/core';
+
+@Component({
+  selector: 'app-header-component',
+  templateUrl: './header-component.component.html',
+  styleUrls: ['./header-component.component.scss']
+})
+export class HeaderComponentComponent {
+
+  constructor(private renderer: Renderer2, private el: ElementRef) { }
+
+  ngOnInit(): void {
+    const button = this.el.nativeElement.children[0]; // Accede al primer hijo de <body>
+    const nav = document.querySelector("nav");
+    const header = document.querySelector("header");
+
+    this.renderer.listen(button, 'click', (event) => {
+      if (event.target.dataset.menustate === "closed") {
+        this.renderer.setAttribute(event.target, 'data-menustate', 'open');
+        this.renderer.setAttribute(nav, 'data-state', 'open');
+        this.renderer.setAttribute(header, 'data-menustate', 'open');
+      } else {
+        this.renderer.setAttribute(event.target, 'data-menustate', 'closed');
+        this.renderer.setAttribute(nav, 'data-state', 'closed');
+        this.renderer.setAttribute(header, 'data-menustate', 'closed');
+      }
+    });
+  }
+
+}
+
