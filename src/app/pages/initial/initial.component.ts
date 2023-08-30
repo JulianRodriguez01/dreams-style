97ec0e7b602329e6fe0a6559a4059f33b76ba9f2
julianrodriguez01 <julian.rodriguez21@uptc.edu.co>
2023-08-30 18:02:55 -0500

    30/08/23 6:00

--- src/app/pages/initial/initial.component.ts
+++ src/app/pages/initial/initial.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./initial.component.scss']
 })
 export class InitialComponent {
-
+  
+  carouselItems: any[] = []; // Define carouselItems como un arreglo vacío o según tu estructura de datos
+  interval = 5000;
 }
