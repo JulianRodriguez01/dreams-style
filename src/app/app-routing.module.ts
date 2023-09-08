bcbeb934b288390e2017184cda384578cd37f1b7
julianrodriguez01 <julian.rodriguez21@uptc.edu.co>
2023-09-07 19:45:27 -0500

    create login v3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { InitialComponent } from './pages/initial/initial.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'index', component: InitialComponent },
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
