ee46b7725f2dd1189c23e57ebe93fcc6b1846532
AlexaInfante <52977709+AleInfante@users.noreply.github.com>
2023-09-21 12:26:54 -0500

    accountviewa commit

---
+++ src/app/pages/recovery-account/recovery-account.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RecoveryAccountComponent } from './recovery-account.component';
+
+describe('RecoveryAccountComponent', () => {
+  let component: RecoveryAccountComponent;
+  let fixture: ComponentFixture<RecoveryAccountComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [RecoveryAccountComponent]
+    });
+    fixture = TestBed.createComponent(RecoveryAccountComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/recovery-account/recovery-account.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recovery-account',
+  templateUrl: './recovery-account.component.html',
+  styleUrls: ['./recovery-account.component.scss']
+})
+export class RecoveryAccountComponent {
+
+}
