ea234f1df4f13d3280fdb6da86050597c1d0e7ba
julianrodriguez01 <julian.rodriguez21@uptc.edu.co>
2023-11-08 22:47:16 -0500

    connect back register 2

--- src/app/initial/pages/register-user/register-user.component.ts
+++ src/app/initial/pages/register-user/register-user.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ApiPersonService } from '../../../services/api-person.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-user',
@@ -9,8 +10,9 @@ import { ApiPersonService } from '../../../services/api-person.service';
 export class RegisterUserComponent {
 
   data: any = { };
+  isLoading = false;
 
-  constructor(private apiPersonService: ApiPersonService) { }
+  constructor(private apiPersonService: ApiPersonService, private router: Router) { }
 
   convertToText(value: number): void {
     this.data.document_number = value.toString();
@@ -20,17 +22,23 @@ export class RegisterUserComponent {
     console.log('Datos enviados al servidor:', this.data);
     if (this.data.password !== this.data.confirmPassword) {
       console.error('Las contraseñas no coinciden');
+      this.isLoading = false;
       return;
     }
+
     this.apiPersonService.registerUser(this.data).subscribe(
       (response) => {
         console.log('Usuario registrado con éxito', response);
+        this.isLoading = true;
+        this.router.navigate(['/login']);
         alert('Usuario creado exitosamente');
       },
       (error) => {
         console.error('Error al registrar usuario', error);
         alert('Error al registrar usuario');
       }
-    );
+    ).add(() => {
+      this.isLoading = false;
+    });
   }
 }
